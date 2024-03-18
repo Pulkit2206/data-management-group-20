240478b50d3f6d6c388234728b2d69a253bacbe2
Pulkit2206 <marwahapulkit22@gmail.com>
2024-03-18 17:01:06 +0000

    changes in load data file

--- R/Load_data.R
+++ R/Load_data.R
@@ -3,4 +3,5 @@ library(RSQLite)
 
 mock_data <- readr::read_csv("data_upload/final_ecommerce.csv")
 my_connection<-RSQLite::dbConnect(RSQLite::SQLite(),"database/database.db")
-RSQLite::dbWriteTable(my_connection,"customers",mock_data)
+dbWriteTable(my_connection, "customers", mock_data, append = TRUE, row.names = FALSE)
+dbDisconnect(my_connection)
